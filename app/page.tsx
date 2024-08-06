26d260498fca3d7b23922e98fddeb803d4a40c1f
Ricardo Nogueira <ricardo.filipe.nogueira@gmail.com>
2024-08-06 16:58:26 +0100

    feat: add header and header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,36 @@
-import { Button } from "./_components/ui/button";
+import { SearchIcon } from "lucide-react"
+import Header from "./_components/header"
+import { Button } from "./_components/ui/button"
+import { Input } from "./_components/ui/input"
+import Image from "next/image"
 
-export default function Home() {
+const Home = () => {
   return (
-    <Button> Ola</Button>
-  );
+    <div>
+      {/*  header  */}
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Ola, Ricardo!</h2>
+        <p>Segunda-feira, 05 de agosto.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Pesquisar..." />
+          <Button>
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            alt="Agende nos melhores com Hair Reserve"
+            src="/Banner01.png"
+            fill
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
+
+export default Home
